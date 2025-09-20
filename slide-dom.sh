5326c84170758bf5a74fc756525e4df6a1ecfcfd
Your Name <kdresdell@gmail.com>
2025-09-20 13:29:04 -0400

    iCoding on my pi4

--- slide-dom.sh
+++ slide-dom.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-feh --slideshow-delay 5 --fullscreen --auto-zoom --randomize /mnt/pixelparty/Photos_fête_Valou/
+# feh --slideshow-delay 5 --fullscreen --auto-zoom --randomize /mnt/pixelparty/Photos_fête_Valou/
+
+#mpv --fullscreen \
+#    --image-display-duration=5 \
+#    --vo=gpu \
+#    --gpu-context=wayland \
+#    --osd-level=0 \
+#    --shuffle \
+#    --loop-playlist=inf \
+#    --blend-subtitles \
+#    --interpolation \
+#    /mnt/pixelparty/Photos_fête_Valou/*
+
+
+imv-wayland -f -t 5 /mnt/pixelparty/Photos_fête_Valou/*
+# imv-wayland -f -t 5 $(ls /mnt/pixelparty/Photos_fête_Valou/* | shuf)
